5722a3bb3bebe8bf818185d0b3c53d937f97a2ea
Mauro Bazan <mlbkorio@gmail.com>
2018-01-12 02:18:48 -0300

    First Commit

---
+++ VirtualMindWebTest/Controllers/UserController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VirtualMindWebTest.VirtualMindService;
+
+namespace VirtualMindWebTest.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            VirtualMindServiceClient client = new VirtualMindServiceClient();
+
+            Money dolar = client.Cotizacion("Dolar").item;
+            ViewBag.DolarVenta = dolar.valueToSell;
+            ViewBag.DolarCompra = dolar.valueToBuy;
+            ViewBag.DolarActualizacion = dolar.lastUpdate;
+
+            ReturnOfUserVNIoeB4G users =  client.Usuarios();
+
+
+            return View(users.list);
+        }
+
+        // GET: User/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: User/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public ActionResult Create(VirtualMindService.User newUser)
+        {
+            try
+            {
+                VirtualMindServiceClient client = new VirtualMindServiceClient();
+
+                client.InsertUser(newUser);
+               
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: User/Edit/5
+        public ActionResult Edit(int id)
+        {
+            VirtualMindServiceClient client = new VirtualMindServiceClient();
+
+            var model = client.GetUserById(id);
+           
+            return View( model.item );
+        }
+
+        // POST: User/Edit/5
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            try
+            {
+                VirtualMindServiceClient client = new VirtualMindServiceClient();
+                client.UpdateUser(user);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: User/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                VirtualMindServiceClient client = new VirtualMindServiceClient();
+
+                client.DeleteUser(id);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ VirtualMindWebTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VirtualMindWebTest.MvcApplication" Language="C#" %>
